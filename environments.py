a2b84b07cfc4751188c47cb87a9564b8288ae998
Bastiche <bastien.velitchkine@live.fr>
2022-03-06 20:43:56 +0100

    [+] Created the environment

--- environments.py
+++ environments.py
@@ -1,189 +1,198 @@
-import pandas as pd
 import numpy as np
-import random
-from collections import deque
 
-class BaseEnvironment():
+METRICS = ["open", "high", "low", "close", "volume ETH", "volume USDT", "tradecount"]
+
+class BaseEnv():
     '''
     A base reinforcement learning environment to build more complex
     environments uppon
     '''
-    def env_init(self, data, env_info={}):
-        '''
-        Method to initialize the environment
-        '''
-        raise NotImplementedError
-
-    def env_start(self, state):
-        '''
-        A method to take the first step in the environment
-        after it was initialized
-        '''
-        raise NotImplementedError
-
-    def env_step(self, action):
-        '''
-        A method to take a step in the environement (not the first one though)
-        '''
-        raise NotImplementedError
-
-    def env_end(self, reward):
-        '''
-        A method to take the last step in the environment
-        '''
-        raise NotImplementedError
-
-    def env_render(self):
-        '''
-        A method to render the environment to the user
-        '''
-        raise NotImplementedError
-
-    def state(self, loc):
-        '''
-        A helper funtion to yield the current state to the user
-        '''
-        raise NotImplementedError
-
-
-
-class TradingBotEnvironment(BaseEnvironment):
-    '''
-    The main environment of our project. We are developing a trading agent in a crypto
-    environment. Therefore, in this environment, we have information relevant
-    to our agent in the context of crypto-currency trading.
-    '''
-
     def __init__(self) -> None:
-        super().__init__()
-
-    def env_init(self, data, env_info={}):
-        '''
-        Method to initialize the environment
-        '''
-        reward = None
-        observation = None
-        termination = None
-        # see if we really need the line below
-        self.reward_obs_term = (reward, observation, termination)
-        # We define custom parameters below
-        self.lookback_window_size = env_info.get("lookback_window_size",50) # we define the size on our back window
-        self.delta = env_info.get("delta",1) # we define our step-size
-        self.total_steps = env_info.get("total_steps",500) # we define the total steps we will go through
-        self.initial_balance = env_info.get("initial_balance", 1000) # we define our initial balance
-        # Define the different possible actions:
-        # 0 we hold, 1 we buy, 2 we sell
-        self.action_space = np.array([0, 1, 2])
-        # the history of orders = what we have in our portfolio at each step
-        self.orders_history = deque(maxlen=self.lookback_window_size)
-        # the history of the market -- see if we really need this
-        self.market_history = deque(maxlen=self.lookback_window_size)
-        #the size of the state ??
-        self.state_size = (self.lookback_window_size, 10)
-
-        
-    def env_start(self, state):
-        '''
-        Method to take the first step in the environment
-        '''
-        # Called when the episode starts, right before the agent starts. Like a reset
-        reward = 0
-        self.balance = self.initial_balance # what we have in liquidities in our portfolio, in $
-        self.net_worth = self.initial_balance # the total amount of what we own (liquidities + cryptos valuation), in $
-        self.prev_net_worth = self.initial_balance # the total amount of what we owned at previous step
-        self.crypto_held = 0 # the total number of cryptos we own - must be an integer
-        self.crypto_sold = 0
-        self.crypto_bought = 0
-        self.start_step = self.lookback_window_size # we look at history from the beginning
-        self.end_step = self.df_total_steps
-        self.current_step = self.start_step # see how to define start_step ?
-
-        for i in reversed(range(self.lookback_window_size)):
-            current_step = self.current_step - i
-            self.orders_history.append([self.balance, self.net_worth, self.crypto_bought, self.crypto_sold, self.crypto_held])
-            # Below we charge our data -- to modify with the naming and format of our real data
-            self.market_history.append([self.df.loc[current_step, 'Open'],
-                                        self.df.loc[current_step, 'High'],
-                                        self.df.loc[current_step, 'Low'],
-                                        self.df.loc[current_step, 'Close'],
-                                        self.df.loc[current_step, 'Volume']
-                                        ])
-
-        state = np.concatenate((self.market_history, self.orders_history), axis=1)
-        return state
-        
-    def next_observation(self):
-        '''
-        Method to return the next observation
-        '''
-        self.market_history.append([self.df.loc[self.current_step, 'Open'],
-                                    self.df.loc[self.current_step, 'High'],
-                                    self.df.loc[self.current_step, 'Low'],
-                                    self.df.loc[self.current_step, 'Close'],
-                                    self.df.loc[self.current_step, 'Volume']
-                                    ]) # here we add the market data of our current step, to modifiy with our real data
-        obs = np.concatenate((self.market_history,self.orders_history),axis=1)
-        return obs
-        
-    def env_step(self,action):
+        pass
+    
+    def step(self):
         '''
         Method to take a step in the environment
         '''
-        self.crypto_bought = 0
-        self.crypto_sold = 0
-        self.current_step += 1
-        
-        current_unit_price = closing_price # MODIFY HERE to get the closing_price with our data. The price should be for one unit of crypto.
-        
-        if action == 0: # we choose not to do anything, nothing happens
-            pass
-        
-        elif action == 1 and self.balance > current_unit_price:  #MODIFY HERE THE WAY WE BUY. For now: 
-            #Buy one unit of crypto only
-            self.crypto_bought += 1
-            self.balance -= current_unit_price
-            self.crypto_held += self.crypto_bought
-            
-        elif action == 2: # MODIFY HERE THE WAY WE SELL. For now:
-            #We sell one unit of crypto only
-            self.crypto_sold += 1
-            self.balance += current_unit_price
-            self.crypto_held -= self.crypto_sold
-            
-        self.prev_net_worth = self.current_net_worth
-        self.current_net_worth = self.balance + self.crypto_held*current_unit_price 
-        
-        self.orders_history.append([self.balance,self.current_net_worth,self.crypto_bought,self.crypto_sold,self.crypto_held])
-        # we define our reward as the benefit we made since previous step
-        reward = self.current_net_worth - self.prev_net_worth
-        
-        if self.net_worth <= self.initial_balance/2 or self.current_step >= self.total_steps:
-        # We decide to stop if we have lost half our initial money.
-            done = True
-        else:
-            done = False
-        
-        obs = self.next_observation()
-        
-        return obs,reward,done
-            
-    # render environement
-    def env_render(self):
-        '''
-        Method to render the environment to the user
-        '''
-        print(f'Step: {self.current_step}, Net Worth: {self.net_worth}')
+        raise NotImplementedError
 
-    def env_end(self, reward):
+    def reset(self):
         '''
-        The last step to take in the environment
+        A public method to reset the environment
         '''
-        # To implement
-        return None
+        raise NotImplementedError
+        
+    def get_reward(self, action):
+        '''
+        A method to get the reward value from a specific action
+        '''
+        raise NotImplementedError
 
-    def state(self, loc):
+class TradingBotEnv(BaseEnv):
+    '''
+    The main environment of our project. We are developing a trading agent in a crypto
+    environment. Therefore, in this environment, we have information relevant
+    to our agent in the context of crypto-currency trading, mostly market prices,
+    exchange rates, ...
+    '''
+
+    def __init__(self, data, metrics=METRICS, lookback_window_size=20):
+        super().__init__()
+        assert "open" in metrics, "You need at least an 'open' price in your metrics"
+        self.lookback_window_size_ = lookback_window_size
+        self.metrics_ = metrics
+        self.market_data_ = data[metrics]
+        self.length_ = len(data)
+        self.current_index_ = 0
+
+        self.action_space_ = np.array([0, 1, 2])
+
+        self.is_done_ = np.zeros(len(self.market_data_), dtype=bool)
+        self.is_done_[-1] = True
+
+        ### Derive some new features from the data
+        self.rolling_mean_ = self.market_data_["open"].rolling(window=self.lookback_window_size_, center=False, min_periods=0).mean()
+        self.rolling_std_ = self.market_data_["open"].rolling(window=self.lookback_window_size_, center=False, min_periods=0).std()
+        self.upper_band_, self.lower_band_ = self.rolling_mean_ + 2 * self.rolling_std_, self.rolling_mean_ - 2 * self.rolling_std_
+
+        ### Create the state dictionnary
+        self.state_dict_ = {}
+        for column_name in self.market_data_.columns:
+            self.state_dict_[column_name] = self.market_data_[column_name]
+        self.state_dict_["rolling_mean"] = self.rolling_mean_
+        self.state_dict_["rolling_std"] = self.rolling_std_
+        self.state_dict_["upper_band"] = self.upper_band_
+        self.state_dict_["lower_band"] = self.lower_band_
+        self.state_dict_["price_over_sma"] = self.market_data_["open"]/self.rolling_mean_
+
+        self.metrics_ += ["rolling_mean", "rolling_std", "upper_band", "lower_band", "price_over_sma"]
+        
+    def step(self):
         '''
-        A method that returns the state of the environment
+        Method to take a step in the environment
         '''
-        # To implement
-        return None
+        is_done = self.is_done_[self.current_index_]
+        observation = []
+        for metric in self.metrics_:
+            observation.append(self.state_dict_[metric][self.current_index_])
+        if not is_done:
+            self.current_index_ += 1
+        return is_done, observation
+
+    def get_reward(self, action):
+        '''
+        A method to get the reward value from a specific action
+        '''
+        assert action in self.action_space_, f"You cannot take an action that's not one of: {self.action_space_}"
+        a = 0
+        if action == 1:
+            a = 1
+        elif action == 2:
+            a = -1
+            
+        price_t = self.get_current_price()
+        price_t_minus_1 = self.get_price_at(self.current_index_ - 1)
+        price_t_minus_n = self.get_price_at(self.current_index_ - self.lookback_window_size_)
+        
+        return (1 + a*(price_t - price_t_minus_1)/price_t_minus_1)*price_t_minus_1/price_t_minus_n
+
+    def reset(self):
+        '''
+        A public method to reset the environment
+        '''
+        self.current_index_ = 0
+
+    def get_current_price(self):
+        '''
+        A method to get the current opening price
+        '''
+        return self.state_dict_["open"][self.current_index_]
+    
+    def get_final_price(self):
+        '''
+        A method to get the final opening price
+        '''
+        return self.state_dict_["open"][-1]
+    
+    def get_price_at(self, index):
+        '''
+        A method to get the opening price at a given index
+        '''
+        if index < 0:
+            return self.state_dict_["open"][0]
+        if index >= self.length_:
+            return self.get_final_price()
+        return self.state_dict_["open"][index]
+
+    def get_metrics(self, metrics=None):
+        '''
+        A method to get the metrics at the current index
+        '''
+        if not metrics:
+            metrics = self.metrics_
+        return np.array([self.state_dict_[metric][self.current_index_] for metric in metrics])
+
+    def plot(self, metrics=None):
+        '''
+        A method to plot the chosen metrics with matplotlib
+        '''
+        import matplotlib.pyplot as plt
+
+        if not metrics:
+            metrics = self.metrics_
+
+        plt.figure()
+        for metric in metrics:
+            ax = self.state_dict_[metric].plot()
+
+        ax.legend(metrics)
+        plt.show()
+
+if __name__ == "__main__":
+    from loader import TradingDataLoader
+    
+    ## TEST ENVIRONMENT METHODS ##
+
+    data = TradingDataLoader().data()
+    env = TradingBotEnv(data)
+
+    # # env.step()
+    # is_done = False
+    # i = 0
+    # while not(is_done):
+    #     is_done, obs = env.step()
+    #     if i < 3:
+    #         print(obs)
+    #     i += 1
+    # print(f"\nThe operation successfully ended after {i} steps")
+
+    # # env.get_reward()
+    # r1 = env.get_reward(0)
+    # r2 = env.get_reward(1)
+    # r3 = env.get_reward(2)
+    # print(r1, r2, r3)
+    # env.get_reward(67) # Should raise an error
+
+    # # env.reset()
+    # env.reset()
+    # print(env.current_index_ == 0)
+
+    # # env.get_current_price()
+    # for _ in range(10):
+    #     _ = env.step()
+    # print(env.get_current_price())
+    
+    # # env.get_final_price()
+    # print(env.get_final_price())
+    
+    # # env.get_price_at(8000)
+    # print(env.get_price_at(8000))
+
+    # # env.get_metrics()
+    # temp = env.get_metrics()
+    # print(temp)
+    # temp = env.get_metrics(metrics=["open", "close"])
+    # print(temp)
+
+    # env.plot()
+    env.plot(metrics=["open", "close"])
