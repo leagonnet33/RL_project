9af169dd94d6c691e0c19d7c00c9c9d7eb763851
Bastiche <bastien.velitchkine@live.fr>
2022-03-22 15:00:16 +0100

    [*] Refactored run_agent, changed params and strings

--- trainers.py
+++ trainers.py
@@ -1,35 +1,32 @@
-from environments import *
-from portfolio import *
-from loader import TradingDataLoader
-from random import *
-import numpy as np
-from utils import argmax
+'''
+The file that contains the training utility functions
+'''
 
-def run_agent(agent,initial_cash,epsilon,metric):
-    env_info = {}
-    all_averages = []
-    data = TradingDataLoader().data()
-    env = TradingBotEnv(data,metrics = metric)
-    portfolio = Portfolio(portfolio_cash = initial_cash)
-    is_done = False 
-    agent_info = {"num_actions": 2, "epsilon": epsilon}
+def run_agent(agent, environment, portfolio):
+    '''
+    A function that trains any agent in any environment with any portfolio
+    '''
+    is_done = False
     action = agent.agent_start(state=None)
-    i=0
+    i = 0
     while not is_done:
-        i+=1
-        portfolio.apply_action(env.get_current_price(),action)
-        is_done, state = env.step()
-        current_reward = env.get_reward(action)
+        i += 1
+        portfolio.apply_action(environment.get_current_price(), action)
+        is_done, state = environment.step()
+        current_reward = environment.get_reward(action)
+
         if i%1000 == 1:
-            print("iteration:" + str(i))
-            print("Current holdings:", portfolio.get_current_holdings(env.get_current_price()))
-            print("Reward:", env.get_reward(action))
-            print("Current Price:", env.get_price_at(i))
-        current_price = env.get_price_at(i)
+            print(f"Iteration: {i}\n")
+            print(f"Current holdings:\n{portfolio.get_current_holdings(environment.get_current_price())}\n")
+            print(f"Reward: {environment.get_reward(action)}\n")
+            print(f"Current Price: {environment.get_price_at(i)}\n")
+
+        current_price = environment.get_price_at(i)
         current_cash = portfolio.portfolio_cash_
         current_coins = portfolio.portfolio_coin_
         action = agent.agent_step(current_reward,state,current_cash,current_coins,current_price)
+
         if i%1000 == 1:
-            print("action: ", action)
-    
-    print("Final holdings:", portfolio.get_current_holdings(env.get_current_price()))
+            print(f"Action: {action}\n")
+        
+        print(f"Final holdings: {portfolio.get_current_holdings(environment.get_current_price())}")
