973f271f257c5968795039841286571b6acaaa56
Bastiche <bastien.velitchkine@live.fr>
2022-03-28 13:52:04 +0200

    ➕ Added a random color generator

--- utils.py
+++ utils.py
@@ -15,3 +15,12 @@ def argmax(random_generator, q_values):
         
     index = random_generator.choice(ties)
     return index
+
+def generate_rgb_color(random_generator):
+    '''
+    A function that generates RGB alpha values to be used as
+    color a color kwarg in a matplotlib function.
+    '''
+    values = random_generator.random(size=4)
+    return (values[0], values[1], values[2], .5 + values[3]/2) # Alpha should not be too small
+    
